5721c5445ce7f2d0fc4c26c17a10ea0a650dd8cb
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-01-30 21:09:42 +0100

    removed white spaces

--- e-commerce/src/components/OrderItem.jsx
+++ e-commerce/src/components/OrderItem.jsx
@@ -6,10 +6,12 @@ export const OrderItem = ({ order: { products } }) => {
         <>
             {products.map(({ quantity, product: { _id, name, price } }) => (
                 <tr key={_id} className="bg-white dark:bg-gray-800">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <th scope="row" className="px-6 py-4 font-medium 
+                    text-gray-900 whitespace-nowrap dark:text-white">
                         {_id}
                     </th>
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <th scope="row" className="px-6 py-4 font-medium 
+                    text-gray-900 whitespace-nowrap dark:text-white">
                         {name}
                     </th>
                     <td className="px-6 py-4">
