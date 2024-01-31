4982d668ceb8acd3965018034264f1c9d4d4e2b2
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-01-31 20:36:13 +0100

    Update order item heading to display order ID

--- e-commerce/src/components/OrderItem.jsx
+++ e-commerce/src/components/OrderItem.jsx
@@ -5,7 +5,7 @@ export const OrderItem = ({ order }) => {
 
     return (
         <>
-            <h2 className='m-2'>Order: {orderId}</h2>
+            <h2 className='m-2'>Order ID: {orderId}</h2>
             {products.map(({ quantity, product: { _id, name, price } }) => (
                 <tr key={_id} className="bg-white dark:bg-gray-800 border-b-2 border-gray-200">
                     <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
