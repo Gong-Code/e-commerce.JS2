29059858b8b25c8f269c72fe6fac10c07f97a501
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-01-31 20:35:11 +0100

    Refactor OrderItem component to destructure orderId from order prop

--- e-commerce/src/components/OrderItem.jsx
+++ e-commerce/src/components/OrderItem.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 
-export const OrderItem = ({ order: { products } }) => {
+export const OrderItem = ({ order }) => {
+    const { _id: orderId, products } = order;
 
     return (
         <>
+            <h2 className='m-2'>Order: {orderId}</h2>
             {products.map(({ quantity, product: { _id, name, price } }) => (
-                    <tr key={_id} className="bg-white dark:bg-gray-800 border-b border-gray-200">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {_id}
-                        </th>
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {name}
-                        </th>
-                        <td className="px-6 py-4">
-                            {quantity}
-                        </td>
-                        <td className="px-6 py-4">
-                            {price} SEK
-                        </td>
-                    </tr>
+                <tr key={_id} className="bg-white dark:bg-gray-800 border-b-2 border-gray-200">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {_id}
+                    </th>
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {name}
+                    </th>
+                    <td className="px-6 py-4">
+                        {quantity}
+                    </td>
+                    <td className="px-6 py-4">
+                        {price} SEK
+                    </td>
+                </tr>
             ))}
-        </>    
+        </>        
     )
 }
