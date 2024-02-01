e5a6abd7c4a9c8ccf1a58f07cb04505f4f675a51
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-02-01 11:34:27 +0100

    Add total quantity to OrderItem component

--- e-commerce/src/components/OrderItem.jsx
+++ e-commerce/src/components/OrderItem.jsx
@@ -4,7 +4,8 @@ export const OrderItem = ({ order }) => {
     const { _id: orderId, products } = order;
 
     const totalPrice = products.reduce((total, { quantity, product: { price } }) => total + quantity * price, 0);
-    
+    const totalQuantity = products.reduce((total, { quantity }) => total + quantity, 0);
+
     return (
         <>
             <h2 className='text-xl mb-1 mt-4'>Order ID: {orderId}</h2>
@@ -24,7 +25,10 @@ export const OrderItem = ({ order }) => {
                     </td>
                 </tr>
             ))}
-            <h3 className='text-emerald-400 mt-1'>Total Price: {totalPrice} SEK</h3>
+            <div className='flex gap-5 items-center m-2 text-white font-bold'>
+                <p>Total Quantity: {totalQuantity}</p>
+                <p className=''>Total Price: {totalPrice} SEK</p>
+            </div>
         </>        
     )
 }
