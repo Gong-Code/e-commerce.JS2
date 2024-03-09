e6239f48a1195eaffd9b45855267fc9dfba68f9a
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-03-09 15:15:13 +0100

    Remove unnecessary line in userModel.js

--- server-side/models/userModel.js
+++ server-side/models/userModel.js
@@ -2,7 +2,6 @@ const User = require('../schemas/userSchema')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-
 exports.registerUser = async (req, res) => {
     const { email, password } = req.body;
 
