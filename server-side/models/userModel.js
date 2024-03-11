10554bb77f91756aa03633e9c2113c501ef7019e
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-03-11 13:23:12 +0100

    Update JWT token expiration time

--- server-side/models/userModel.js
+++ server-side/models/userModel.js
@@ -17,7 +17,7 @@ exports.registerUser = async (req, res) => {
             password: hashedPassword
         });
 
-        const token = jwt.sign({ userId: user._id }, 'your_jwt_secret');
+        const token = jwt.sign({ _id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
 
         res.status(201).json({ message: 'User created successfully', user, token });
     } catch (err) {
@@ -36,7 +36,7 @@ exports.loginUser = async (req, res) => {
             return res.status(400).json({ message: 'Invalid email or password' });
         }
 
-        const token = jwt.sign({ _id: user._id }, 'your_jwt_secret');
+        const token = jwt.sign({ _id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
 
         res.status(200).json({ message: 'User logged in successfully', token })
 
