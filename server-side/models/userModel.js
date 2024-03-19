9df727e2e73bc1f6c65f8800ee66924ef0fa9c2a
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-03-19 15:18:17 +0100

    Update token expiration to 1hr in userModel.js

--- server-side/models/userModel.js
+++ server-side/models/userModel.js
@@ -17,9 +17,9 @@ exports.registerUser = async (req, res) => {
             password: hashedPassword
         });
 
-        const token = jwt.sign({ _id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+        const token = jwt.sign({ _id: user._id }, 'your_jwt_secret', { expiresIn: '1hr' });
 
-        res.status(201).json({ message: 'User created successfully', user, token });
+        res.status(201).json({ message: 'User created successfully', user, token});
     } catch (err) {
         console.log('Failed to create a user', err.message);
         res.status(500).json({ message: 'Failed to create a user', error: err.message });
@@ -36,9 +36,9 @@ exports.loginUser = async (req, res) => {
             return res.status(400).json({ message: 'Invalid email or password' });
         }
 
-        const token = jwt.sign({ _id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+        const token = jwt.sign({ _id: user._id }, 'your_jwt_secret', { expiresIn: '1hr' });
 
-        res.status(200).json({ message: 'User logged in successfully', token })
+        res.status(200).json({ message: 'User logged in successfully', token})
 
     } catch (err) {
         console.log('Failed to log in', err.message);
