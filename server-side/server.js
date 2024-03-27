6d9449d60f4bb0e4df0f1e05ddef9ea48fee18c8
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-03-27 13:25:47 +0100

    Remove default route handler in server.js

--- server-side/server.js
+++ server-side/server.js
@@ -9,10 +9,6 @@ app.listen(PORT, () => {
 
 require('dotenv').config();
 
-app.get('/', (req, res) => {
-    res.send('Hey this is my API running 🥳')
-})
-
 mongoose
 .connect(process.env.MONGO_URI)
 .then(() => {
